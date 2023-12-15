c02c00935e9e096ed894ee884d923220c66bc0e4
Zhanpeng Tong <tong.zh@northeastern.edu>
2023-12-15 00:30:30 -0500

    fix1

--- src/psp/client.js
+++ src/psp/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_BASE_URL;
+const API_BASE = process.env.REACT_APP_API_BASE_URL || "http://localhost:4000";
 const USERS_API = `${API_BASE}/api/users`;
 const ITEMS_API = `${API_BASE}/api/items`;
 const REVIEWS_API = `${API_BASE}/api/reviews`;
